83cda445cea82aea517aa4783937b2d3b9daaae7
Shoji Moto <shmoto1@gmail.com>
2017-11-04 04:02:05 -0500

    Timer and handMovements

--- handy.py
+++ handy.py
@@ -1,4 +1,4 @@
-import os, sys, inspect, thread, time
+import os, sys, inspect, thread, time, timeit
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 # Windows and Linux
 # arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
@@ -8,10 +8,30 @@ sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
 import Leap
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
 
+def handMovements(frame):
+	for hand in frame.hands:
+
+            handType = "Left hand" if hand.is_left else "Right hand"
+
+            print "  %s, id %d, position: %s" % (
+                handType, hand.id, hand.palm_position)
+
+            # Get the hand's normal vector and direction
+            normal = hand.palm_normal
+            direction = hand.direction
+
+            # Calculate the hand's pitch, roll, and yaw angles
+            print "  pitch: %f degrees, roll: %f degrees, yaw: %f degrees" % (
+                direction.pitch * Leap.RAD_TO_DEG,
+                normal.roll * Leap.RAD_TO_DEG,
+                direction.yaw * Leap.RAD_TO_DEG)
+
+startTime = 0
+beforeTime = 0
 
 class LeapEventListener(Leap.Listener):
 
-    global count = 0
+    #global count = 0
     def on_init(self, controller):
         print "Initialized"
     def on_connect(self, controller):
@@ -27,11 +47,18 @@ class LeapEventListener(Leap.Listener):
     def on_frame(self, controller):
         frame = controller.frame()
         #Process frame data
-        if count == 9:
-            print "Timer: " + str(int(frame.timestamp) / int(1000000))
-            count = 0
-        else:
-            count += 1
+
+        #Show time elapsed
+        global startTime
+        global beforeTime
+        if startTime == 0:
+        	startTime = timeit.default_timer()
+        if int ((timeit.default_timer() - startTime) * 10) % 10 == 0:
+        	if beforeTime != int(timeit.default_timer() - startTime):
+        		print "Elapsed {}".format(int(timeit.default_timer() - startTime))
+        		beforeTime = int(timeit.default_timer() - startTime)
+
+        handMovements(frame)
 
     def on_exit(self, controller):
         print "Exited"
@@ -41,7 +68,7 @@ def main():
 	listener = LeapEventListener()
 	controller = Leap.Controller()
 	controller.add_listener(listener)
-    # Keep this process running until Enter is pressed
+	# Keep this process running until Enter is pressed
 	print "Press Enter to quit..."
 	try:
 		sys.stdin.readline()
@@ -49,4 +76,4 @@ def main():
 		pass
 
 if __name__ == "__main__":
-    main()
+	main()
